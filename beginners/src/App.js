b08ab9ccf44b9c408a173be16335fe8338614017
유지은 <wlwl0219@humaxparcs.com>
2023-03-03 17:25:30 +0900

    useEffect update, clean test

--- beginners/src/App.js
+++ beginners/src/App.js
@@ -1,18 +1,56 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const [showing, setShowing] = useState(false);
+  const onShowClick = () => setShowing((prev) => !prev);
+
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
 
   console.log("i run all the time");
+
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("I run only once.");
   }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
+
+      <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onShowClick}>{showing ? "Hide" : "Show"}</button>
+      </div>
     </div>
   );
 }
