cf9eb1947b48be4d5a4d1eae72dd37f15b5a6196
유지은 <wlwl0219@humaxparcs.com>
2023-03-03 12:12:23 +0900

    useEffect one call test

--- beginners/src/App.js
+++ beginners/src/App.js
@@ -1,5 +1,20 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div>hi</div>;
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API....");
+  }, []);
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
 }
 
 export default App;
