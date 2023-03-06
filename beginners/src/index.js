15c751fb31a6838f3bb9c7e5a40150afc4f4091f
유지은 <wlwl0219@humaxparcs.com>
2023-03-06 10:46:15 +0900

    create coin list

--- beginners/src/index.js
+++ beginners/src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Todo from "./Todo";
+import Coin from "./Coin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Todo />
+    <Coin />
   </React.StrictMode>
 );
